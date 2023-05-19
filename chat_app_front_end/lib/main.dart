eac38e2d2bcf8797d23915629f5ae65aa4d9990b
Franckay-kim <fkim6322@gmail.com>
2023-05-19 11:17:01 +0300

    message

--- chat_app_front_end/lib/main.dart
+++ chat_app_front_end/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_auth/Screens/Welcome/welcome_screen.dart';
 import 'package:flutter_auth/constants.dart';
 import 'package:flutter_auth/messages.dart';
+import 'package:flutter/scheduler.dart';
 
 void main() => runApp(const MyApp());
 
