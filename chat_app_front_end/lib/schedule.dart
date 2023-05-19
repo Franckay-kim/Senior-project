ac14fdb3e429aa3d2dd01daa487ae59e0af1328d
Franckay-kim <fkim6322@gmail.com>
2023-05-19 12:04:22 +0300

    message

--- chat_app_front_end/lib/schedule.dart
+++ chat_app_front_end/lib/schedule.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
