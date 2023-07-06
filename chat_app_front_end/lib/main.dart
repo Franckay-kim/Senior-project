6bc7de27f5db6e5cccd7371e4c169f627e8e9f57
Franckay-kim <fkim6322@gmail.com>
2023-07-06 22:09:29 +0300

    savings recent changes

--- chat_app_front_end/lib/main.dart
+++ chat_app_front_end/lib/main.dart
@@ -7,7 +7,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    // TODO: Replace credentials with your own
+    //TODO: Replace credentials with your own
     url: 'https://lvpjqqiicmztxjpdbgdz.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2cGpxcWlpY216dHhqcGRiZ2R6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg2MDA2NzEsImV4cCI6MjAwNDE3NjY3MX0.cF8vWd-cgMED4DM6WK19r69VM_uXrrMXb7guyDxJq7U',
   );
